d061e31160b5a8ae16505563964e94469e1b6e78
konkarin <konkarin.photo@gmail.com>
2023-04-05 18:49:45 +0900

    i18n-composable版

---
+++ pages/composable.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>{{ t("welcome") }} composable</div>
+</template>
+
+<script>
+import { defineComponent } from "@nuxtjs/composition-api";
+import { useI18n } from "vue-i18n-composable";
+
+export default defineComponent({
+  setup() {
+    return {
+      ...useI18n(),
+    };
+  },
+});
+</script>
--- plugins/i18n.js
+++ plugins/i18n.js
@@ -1,13 +1,14 @@
 import Vue from "vue";
 import VueI18n from "vue-i18n";
+import { createI18n } from "vue-i18n-composable";
 // import { createI18n } from "vue-i18n-bridge";
 
 // Vue.use(VueI18n, { bridge: true }); // you must specify '{ bridge: true }' plugin option when install vue-i18n
 
-Vue.use(VueI18n);
+// Vue.use(VueI18n);
 
 export default ({ app }, inject) => {
-  const i18n = new VueI18n({
+  const i18n = createI18n({
     locale: "ja",
     messages: {
       en: {
